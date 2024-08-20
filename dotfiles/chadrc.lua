82f84681bdeb5ab730e6bc1ac4f91162c6a5f335
ioo <129602810+io39@users.noreply.github.com>
2024-08-20 10:35:39 +0200

    add nvim configuration

---
+++ dotfiles/chadrc.lua
@@ -0,0 +1,25 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "bearded-arc",
+  theme_toggle = { "bearded-arc", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+    overriden_modules = nil,
+  },
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
